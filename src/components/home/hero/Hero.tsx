b2e1215e62be48975261d01a04b1ad408e7dcc86
Thomas Cazade <cazade.thomas@gmail.com>
2020-02-11 22:40:00 +0100

    fix(hero): better responsive on large screens, enable styled-components/macro

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 import HeroText from './HeroText';
 import InviteCard from './InviteCard';
@@ -7,6 +7,7 @@ import Wave from './Wave';
 
 const RelativeContainerEl = styled.div`
   position: relative;
+  background: linear-gradient(0deg, rgb(35, 39, 45) 0%, rgba(75,80,89,1) 100%);
 `;
 
 const ContainerEl = styled.div`
@@ -17,7 +18,6 @@ const ContainerEl = styled.div`
   padding-top: 6rem;
   padding-left: 2rem;
   padding-right: 2rem;
-  background: linear-gradient(0deg, rgb(35, 39, 45) 0%, rgba(75,80,89,1) 100%);
 
   @media (min-width: 992px) {
     flex-direction: row;
@@ -25,18 +25,14 @@ const ContainerEl = styled.div`
   }
 
   @media (min-width: 1366px) {
-    padding-left: 4rem;
-    padding-right: 4rem;
-  }
-
-  @media (min-width: 1440px) {
-
+    max-width: 1200px;
+    margin: 0 auto;
+    padding-left: 0;
+    padding-right: 0;
   }
 
   @media (min-width: 1920px) {
     min-height: 70vh;
-    padding-left: 6rem;
-    padding-right: 6rem;
   }
 `;
 
@@ -73,8 +69,9 @@ const InviteCardContainerEl = styled.div`
     padding: 0;
   }
 
-  @media (min-width: 1920px) {
-    padding: 0 10rem;
+  @media (min-width: 1366px) {
+    padding-left: 0;
+    padding-right: 0;
   }
 `;
 
